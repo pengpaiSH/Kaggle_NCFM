dec6c75b24f4a88afd25a152f8adc162bf5a8ddd
Pai Peng <pengpai_sh@163.com>
2016-12-02 15:55:59 +0800

    Add train.py and split_train_val.py
    Add train.py and split_train_val.py

---
+++ split_train_val.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import shutil
+
+np.random.seed(2016)
+
+root_train = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/train_split'
+root_val = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/val_split'
+
+root_total = '/data1/home/waynema/popeyepeng/NCFM/data/train'
+
+FishNames = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+
+nbr_train_samples = 0
+nbr_val_samples = 0
+
+# Training proportion
+split_proportion = 0.8
+
+for fish in FishNames:
+    if fish not in os.listdir(root_train):
+        os.mkdir(os.path.join(root_train, fish))
+
+    total_images = os.listdir(os.path.join(root_total, fish))
+
+    nbr_train = int(len(total_images) * split_proportion)
+
+    np.random.shuffle(total_images)
+
+    train_images = total_images[:nbr_train]
+
+    val_images = total_images[nbr_train:]
+
+    for img in train_images:
+        source = os.path.join(root_total, fish, img)
+        target = os.path.join(root_train, fish, img)
+        shutil.copy(source, target)
+        nbr_train_samples += 1
+
+    if fish not in os.listdir(root_val):
+        os.mkdir(os.path.join(root_val, fish))
+
+    for img in val_images:
+        source = os.path.join(root_total, fish, img)
+        target = os.path.join(root_val, fish, img)
+        shutil.copy(source, target)
+        nbr_val_samples += 1
+
+print('Finish splitting train and val images!')
+print('# training samples: {}, # val samples: {}'.format(nbr_train_samples, nbr_val_samples))
+print(x, y)
---
+++ train.py
@@ -0,0 +1,80 @@
+from inception_v3 import InceptionV3, preprocess_input
+import os
+from keras.layers import Flatten, Dense, AveragePooling2D
+from keras.models import Model
+from keras.optimizers import RMSprop, SGD
+from keras.callbacks import ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+
+learning_rate = 0.0001
+img_width = 299
+img_height = 299
+nbr_train_samples = 3019
+nbr_validation_samples = 758
+nbr_epochs = 50
+batch_size = 32
+
+train_data_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/train_split'
+val_data_dir = '/Users/pengpai/Desktop/python/DeepLearning/Kaggle/NCFM/data/val_split'
+
+FishNames = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+
+print('Loading InceptionV3 Weights ...')
+InceptionV3_notop = InceptionV3(include_top=False, weights='imagenet',
+                    input_tensor=None, input_shape=(299, 299, 3))
+# Note that the preprocessing of InceptionV3 is:
+# (x / 255 - 0.5) x 2
+
+print('Adding Average Pooling Layer and Softmax Output Layer ...')
+output = InceptionV3_notop.get_layer(index = -1).output  # Shape: (8, 8, 2048)
+output = AveragePooling2D((8, 8), strides=(8, 8), name='avg_pool')(output)
+output = Flatten(name='flatten')(output)
+output = Dense(8, activation='softmax', name='predictions')(output)
+
+InceptionV3_model = Model(InceptionV3_notop.input, output)
+#InceptionV3_model.summary()
+
+optimizer = SGD(lr = learning_rate, momentum = 0.9, decay = 0.0, nesterov = True)
+InceptionV3_model.compile(loss='categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
+
+# autosave best Model
+best_model_file = "./weights.h5"
+best_model = ModelCheckpoint(best_model_file, monitor='val_acc', verbose = 1, save_best_only = True)
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.1,
+        zoom_range=0.1,
+        rotation_range=10.,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True)
+
+# this is the augmentation configuration we will use for validation:
+# only rescaling
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size = (img_width, img_height),
+        batch_size = batch_size,
+        shuffle = True,
+        classes = FishNames,
+        class_mode = 'categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        shuffle = True,
+        classes = FishNames,
+        class_mode = 'categorical')
+
+InceptionV3_model.fit_generator(
+        train_generator,
+        samples_per_epoch = nbr_train_samples,
+        nb_epoch = nbr_epochs,
+        validation_data = validation_generator,
+        nb_val_samples = nbr_validation_samples,
+        callbacks = [best_model])
